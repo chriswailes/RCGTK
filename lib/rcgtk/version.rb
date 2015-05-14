ff3b957f32c2460e45d82c4eff21a3886a683088
Chris Wailes <chris.wailes@gmail.com>
2015-05-14 11:56:06 -0400

    Initial work on moving to 3.6

--- lib/rcgtk/version.rb
+++ lib/rcgtk/version.rb
@@ -5,7 +5,7 @@
 
 module RCGTK
 	# The version number of the RCGTK library.
-	VERSION             = '3.5.0'
+	VERSION             = '3.6.0'
 	# The version of LLVM targeted by RCGTK.
-	LLVM_TARGET_VERSION = '3.5'
+	LLVM_TARGET_VERSION = '3.6'
 end
